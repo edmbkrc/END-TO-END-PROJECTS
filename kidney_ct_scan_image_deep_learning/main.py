b3cdaf9f18c523397526872102c00f35597916e8
Adem <admcns45@gmail.com>
2025-08-27 20:31:23 +0300

    requirements added

--- kidney_ct_scan_image_deep_learning/main.py
+++ kidney_ct_scan_image_deep_learning/main.py
@@ -1,3 +1,15 @@
 from src.cnnKidneyDiseaseClassifier import logger 
+from src.cnnKidneyDiseaseClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
